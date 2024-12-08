549c763b96ff8eb35e800a3ee1fcdff1e1db907b
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2024-12-09 01:49:37 +0530

    first commit

---
+++ Logical Problems/Patterns/Mirror_Right_Triangle.js
@@ -0,0 +1,11 @@
+let n= 5;
+
+for(let i = 1 ; i<= n ; i++){
+    for(let k =n-1; k>=i ; k--){
+        process.stdout.write("  ");
+    }
+    for(let j = 1 ; j<= i ; j++){
+        process.stdout.write("* ");
+    }
+    console.log();
+}
---
+++ Logical Problems/Patterns/V_pattern.js
@@ -0,0 +1,23 @@
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= i - 1; k++) {
+        process.stdout.write("  ");
+    }
+    for (let j = i; j <= i; j++) {
+        process.stdout.write("* ");
+    }
+    for (let j = i; j <= n - 1; j++) {
+        process.stdout.write("  ");
+    }
+    for (let o = i; o <= n - 2; o++) {
+        process.stdout.write("  ");
+    }
+    for (let x = i; x <= i; x++) {
+        if (x >= 5) {
+            break
+        }
+        process.stdout.write("* ");
+    }
+    console.log();
+}

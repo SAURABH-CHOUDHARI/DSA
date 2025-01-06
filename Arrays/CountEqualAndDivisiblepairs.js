5a63bd9435214ba4170b41f555577e9a95ca1285
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-01-06 23:42:05 +0530

    Arrays

---
+++ Arrays/CountEqualAndDivisiblepairs.js
@@ -0,0 +1,9 @@
+var countPairs = function (nums, k) {
+    let p = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && i * j % k === 0) p++;
+        }
+    }
+    return p;
+};

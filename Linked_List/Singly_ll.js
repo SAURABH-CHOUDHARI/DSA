426838c3943a0846e32d79b48b120fb11ff430de
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-03 19:27:14 +0530

    minor_singly_ll_size_and Out of bound fixes

--- Linked_List/Singly_ll.js
+++ Linked_List/Singly_ll.js
@@ -17,90 +17,93 @@ class LinkedList {
         newNode.next = this.head
         this.head = newNode
     }
-    insertAtlast(val){
+    insertAtlast(val) {
         this.size++
         let newNode = new Node(val)
-        if(this.head == null){
+        if (this.head == null) {
             this.head = newNode;
             return
         }
-        let cur  = this.head;
-        while(cur.next != null){
-            cur = cur.next 
+        let cur = this.head;
+        while (cur.next != null) {
+            cur = cur.next
         }
-        cur.next= newNode;
-        
+        cur.next = newNode;
+
     }
-    deleteAtfirst(){
-        this.size--
-        if(this.head == null){
+    deleteAtfirst() {
+        if (this.head == null) {
             console.log("no node to delete")
             return
         }
-        if (this.head.next == null){
+        this.size--
+        if (this.head.next == null) {
             this.head = null;
             return
         }
         this.head = this.head.next;
     }
-    deleteAtlast(){
+    deleteAtlast() {
+        if (this.head == null ) {
+            return
+        }
         this.size--
-    if(this.head == null || this.head.next == null){
-        this.head = null
-        return
+        if (this.head.next == null) {
+            this.head = null
+            return
+        }
+        let cur = this.head;
+        while (cur.next.next != null) {
+            cur = cur.next;
+        }
+        cur.next = null
     }
-    let cur = this.head;
-    while(cur.next.next != null){
-        cur = cur.next;
-    }
-    cur.next = null
-    }
-    insertAtPos(val,pos){
-        if(pos<1 || this.size + 2 < pos ){
+    insertAtPos(val, pos) {
+        if (pos < 1 || this.size + 1 < pos) {
             console.log("Out of Bound")
             return
         }
-        if(pos == 1){
+        if (pos == 1) {
             this.insertAtfirst(val)
             return
         }
-        if(pos == this.size+1){
+        if (pos == this.size + 1) {
             this.insertAtlast(val)
             return
         }
         this.size++
         let newNode = new Node(val)
-        let cur  = this.head;
-        let i=1;
-        while(i<pos-1){
+        let cur = this.head;
+        let i = 1;
+        while (i < pos - 1) {
             cur = cur.next;
             i++
         }
         newNode.next = cur.next;
         cur.next = newNode;
     }
-    deleteAtPos(pos){
-        if(pos<1|| this.size < pos){
+    deleteAtPos(pos) {
+        if (pos < 1 || this.size < pos) {
             console.log("Out of bound")
             return
         }
-        if(pos == 1){
+        if (pos == 1) {
             this.deleteAtfirst()
             return
         }
-        if(pos == this.size){
+        if (pos == this.size) {
             this.deleteAtlast()
             return
         }
         this.size--
-        let cur  = this.head;
-        let i=1;
-        while(i<pos-1){
+        let cur = this.head;
+        let i = 1;
+        while (i < pos - 1) {
             cur = cur.next;
             i++
         }
         cur.next = cur.next.next;
-        
+
     }
     print() {
         if (this.head == null) {
@@ -126,9 +129,9 @@ newNode.insertAtFirst(50)
 newNode.insertAtlast(10)
 newNode.deleteAtfirst()
 newNode.deleteAtlast()
-newNode.insertAtPos(31,3)
-newNode.insertAtPos(300,4)
-newNode.insertAtPos(333,6)
+newNode.insertAtPos(31, 3)
+newNode.insertAtPos(300, 4)
+newNode.insertAtPos(333, 6)
 newNode.deleteAtPos(3)
 
 console.log(newNode.size)

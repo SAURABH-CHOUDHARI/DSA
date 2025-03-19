05c6b2340de05a5343352eba99abfb9133d9812a
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-19 21:17:21 +0530

    dequeue operation

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,36 +1,45 @@
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
 }
-class Queue{
-    constructor(){
-        this.front=null;
-        this.rear=null;
-        this.size=0;
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
     }
-    enqueue(val){
+    enqueue(val) {
         const newNode = new Node(val)
-        if(this.rear==null){
+        if (this.rear == null) {
             this.front = this.rear = newNode;
-        }else{
+        } else {
             this.rear.next = newNode;
             this.rear = newNode;
         }
         this.size++;
     }
-    empty(){
-        return this.size===0;
+    dequeue() {
+        if (this.front == null) {
+            console.log("queue is empty")
+            return
+        }
+        this.front = this.front.next;
+        if (this.front == null) this.rear = null;
+        this.size--;
     }
-    display(){
-        if(this.front == null){
+    empty() {
+        return this.size === 0;
+    }
+    display() {
+        if (this.front == null) {
             console.log("queue is empty")
             return
         }
         let cur = this.front
-        while(cur != null){
-            process.stdout.write(cur.val+" ");
+        while (cur != null) {
+            process.stdout.write(cur.val + " ");
             cur = cur.next;
         }
     }
@@ -42,4 +51,12 @@ Q1.enqueue(20);
 Q1.enqueue(30);
 Q1.enqueue(40);
 Q1.display();
-console.log(Q1.empty());
+console.log();
+
+Q1.dequeue();
+Q1.display();
+console.log();
+console.log(Q1.size)
+
+Q1.empty();
+console.log(Q1.empty())

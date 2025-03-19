8da11c62c40a9fe8ef26417ea4b4dabbca25c5f2
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-19 21:14:19 +0530

    queue class constructor

---
+++ Queue/Queue.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class Queue{
+    constructor(){
+        this.front=null;
+        this.rear=null;
+        this.size=0;
+    }
+}

3d6533a7635e7d94222f429f1eb09b72cb365fe7
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-20 13:15:54 +0530

    Stack_constructor

---
+++ Stack/stack.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.size = 0;
+    }
+
+}
+

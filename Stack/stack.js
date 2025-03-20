cd4c067b3f6ea8e3a33c11472f748c9a60617bd7
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-20 13:17:31 +0530

    Stack_pop_peek_display_operations

--- Stack/stack.js
+++ Stack/stack.js
@@ -18,7 +18,34 @@ class Stack {
         this.top = newNode;
     }
 
-    
+    pop() {
+        if (this.size === 0) {
+            return null;
+        }
+
+        const poppedValue = this.top.val;
+        this.top = this.top.next;
+        this.size--;
+        return poppedValue;
+    }
+
+    peek() {
+        if (this.size === 0) {
+            return null;
+        }
+        return this.top.val;
+    }
+
+    display() {
+        let values = [];
+        let cur = this.top;
+        while (cur) {
+            values.push(cur.val);
+            cur = cur.next;
+        }
+
+        console.log(values.reverse().join(' '));
+    }
 }
 
 let s = new Stack();
@@ -26,3 +53,7 @@ s.push(10);
 s.push(20);
 s.push(30);
 s.push(40);
+s.display();
+s.pop();
+s.display();
+console.log(s.peek()); 
